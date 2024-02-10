cc7f0f5b5004644a79165960e9443b68ee566c14
Mafirrr <84912073+Mafirrr@users.noreply.github.com>
2024-02-10 09:13:34 +0700

    Initial commit

---
+++ src/mafira_m1/Mafira_M1.java
@@ -0,0 +1,20 @@
+package mafira_m1;
+
+/**
+ *
+ * @author Mafir
+ */
+public class Mafira_M1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Mafira Aurelia Salsabila Firdaus");
+        System.out.print("NIM E41232457");
+        System.out.println(" " + "Golongan A");
+        System.out.println("Teknik Informatika Kampus Bondowoso");
+    
+    }
+    
+}
